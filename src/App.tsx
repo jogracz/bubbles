ee248ae657efe1d741f49f8afd473e83b25d5d86
Joanna Graczyk <joanna.graczyk1@gmail.com>
2022-05-28 21:00:25 +0200

    Add general styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
-import "./App.css";
 import { About } from "./pages/about";
 import { Bubbles } from "./pages/bubbles";
 import { Badges } from "./pages/badges";
@@ -9,15 +9,27 @@ import { Navigation } from "./components/navigation";
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
+    <StyledApp>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Bubbles />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/badges" element={<Badges />} />
-      </Routes>
-    </div>
+      <StyledContent>
+        <Routes>
+          <Route path="/" element={<Bubbles />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/badges" element={<Badges />} />
+        </Routes>
+      </StyledContent>
+    </StyledApp>
   );
 };
+
+const StyledApp = styled.div`
+  width: "100%";
+  height: "100%";
+  min-height: 100vh;
+`;
+
+const StyledContent = styled.div`
+  padding: 6% 8%;
+`;
 
 export default App;
