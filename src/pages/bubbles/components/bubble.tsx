1a376dbeca78307d549195638ad57a0f0a564f82
Joanna Graczyk <joanna.graczyk1@gmail.com>
2022-05-28 23:25:30 +0200

    Add Bubble component

---
+++ src/pages/bubbles/components/bubble.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
+
+interface BubbleProp {
+  handleAdd: () => void;
+}
+
+export const Bubble: React.FunctionComponent<BubbleProp> = ({ handleAdd }) => {
+  const [popped, setPopped] = useState(false);
+
+  const handleClick = () => {
+    setPopped(true);
+    // handleAdd();
+  };
+
+  return (
+    <StyledBubble
+      onClick={handleClick}
+      popped={popped}
+      left={randomFromRange(1, 80)}
+      top={randomFromRange(1, 80)}
+    >
+      <StyledBubbleInner size={randomFromRange(3, 100)} />
+    </StyledBubble>
+  );
+};
+
+const randomFromRange = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const moveToTopAnimation = keyframes`
+  0% { transform: translateY(-10%);;}
+  50% { transform: translateY(0%)}
+  100% { transform: translateY(-10%); }
+`;
+
+interface BubbleProps {
+  popped: boolean;
+  left: number;
+  top: number;
+}
+const StyledBubble = styled.div`
+  position: absolute;
+  left: ${(props: BubbleProps) => `${props.left}%`};
+  top: ${(props: BubbleProps) => `${props.top}%`};
+  display: ${(props: BubbleProps) => (props.popped ? "none" : "block")};
+  padding: 5px;
+  border-radius: 50%;
+  animation-name: ${moveToTopAnimation};
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-out;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+interface BubbleInnerProps {
+  size: number;
+}
+const StyledBubbleInner = styled.div`
+  width: ${(props: BubbleInnerProps) => `${props.size / 5}vmin`};
+  height: ${(props: BubbleInnerProps) => `${props.size / 5}vmin`};
+  background-color: pink;
+  margin: 5px;
+  border-radius: 50%;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
